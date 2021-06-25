826548446495fdf03d1d7c8df9f2b5679062100e
edo <mdinh@hawk.iit.edu>
2021-06-25 02:18:14 -0500

    update multiple tasks

--- main.py
+++ main.py
@@ -1,25 +1,73 @@
 from flask import Flask, flash, render_template, request, redirect, url_for
-from tasks import add
+from tasks import add, subtract, multiply, divide
 
 app = Flask(__name__)
 app.config.from_pyfile("config.py")
 
 
+def get_data(request):
+    data = {}
+
+    if request.form.get("first").strip() == "" or request.form.get("second").strip == "":
+        return None
+
+    data["first"] = int(request.form.get("first"))
+    data["second"] = int(request.form.get("second"))
+
+    return data
+
+
 @app.route("/", methods=["POST", "GET"])
 def home():
-    if request.method == "GET":
-        return render_template("index.html")
+    return render_template("index.html")
 
-    elif request.method == "POST":
-        data = {}
 
-        data["first"] = int(request.form.get("first"))
-        data["second"] = int(request.form.get("second"))
+@app.route("/add", methods=["POST", "GET"])
+def add_numbers():
+    if request.method == "POST":
+        data = get_data(request)
 
-        result = add.apply_async(args=[data], duration=20)
-        flash(f"Result: {data['first']} + {data['second']} = {result.get()}")
+        if data:
+            result = add.apply_async(args=[data], duration=20)
+            flash(f"Result: {data['first']} + {data['second']} = {result.get()}")
 
-        return redirect(url_for("home"))
+    return redirect(url_for("home"))
+
+
+@app.route("/subtract", methods=["POST", "GET"])
+def subtract_numbers():
+    if request.method == "POST":
+        data = get_data(request)
+
+        if data:
+            result = subtract.apply_async(args=[data], duration=20)
+            flash(f"Result: {data['first']} - {data['second']} = {result.get()}")
+
+    return redirect(url_for("home"))
+
+
+@app.route("/multiply", methods=["POST", "GET"])
+def multiply_numbers():
+    if request.method == "POST":
+        data = get_data(request)
+
+        if data:
+            result = multiply.apply_async(args=[data], duration=20)
+            flash(f"Result: {data['first']} x {data['second']} = {result.get()}")
+
+    return redirect(url_for("home"))
+
+
+@app.route("/divide", methods=["POST", "GET"])
+def divide_numbers():
+    if request.method == "POST":
+        data = get_data(request)
+
+        if data:
+            result = divide.apply_async(args=[data], duration=20)
+            flash(f"Result: {data['first']} ÷ {data['second']} = {result.get()}")
+
+    return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
--- tasks.py
+++ tasks.py
@@ -9,3 +9,25 @@ client.config_from_object("celeryconfig")
 def add(data):
     sleep(5)
     return data["first"] + data["second"]
+
+
+@client.task
+def subtract(data):
+    sleep(5)
+    return data["first"] - data["second"]
+
+
+@client.task
+def multiply(data):
+    sleep(5)
+    return data["first"] * data["second"]
+
+
+@client.task
+def divide(data):
+    sleep(5)
+
+    if data["second"] == 0:
+        return "Error"
+
+    return data["first"] / data["second"]
