754a606b031e3e78601e881ccad9cb12fe5acc89
edo <mdinh@hawk.iit.edu>
2021-06-25 03:21:12 -0500

    Update tasks

--- tasks.py
+++ tasks.py
@@ -1,4 +1,5 @@
 from celery import Celery
+from time import sleep
 
 client = Celery("tasks")
 client.config_from_object("celeryconfig")
@@ -6,21 +7,26 @@ client.config_from_object("celeryconfig")
 
 @client.task
 def add(data):
+    sleep(5)
     return data["first"] + data["second"]
 
 
 @client.task
 def subtract(data):
+    sleep(5)
     return data["first"] - data["second"]
 
 
 @client.task
 def multiply(data):
+    sleep(5)
     return data["first"] * data["second"]
 
 
 @client.task
 def divide(data):
+    sleep(5)
+
     if data["second"] == 0:
         return "Error"
 
