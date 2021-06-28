f9dc72c83ef8c70267a917bf33100529b59ec73c
edo <mdinh@hawk.iit.edu>
2021-06-28 03:29:38 -0500

    Added dockerfile to project

--- config/__init__.py
+++ config/__init__.py
@@ -4,5 +4,5 @@ class Config(object):
     FLASK_SECRET_KEY = "3d6f45a5fc12445dbac2f59c3b6c7cb1"
 
     # celery config
-    CELERY_BROKER_URL = 'redis://localhost:6379/0'
-    CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+    CELERY_BROKER_URL = 'redis://redis:6379/0'
+    CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
